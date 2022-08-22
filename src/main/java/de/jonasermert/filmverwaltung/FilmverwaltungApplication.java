801bbccf0557d71664b97997405e84a249294cc7
Jonas Ermert <ermert.jonas@yahoo.de>
2022-08-22 12:08:59 +0200

    Projekt mit Spring Initializer initalisiert,Datenbank Infos in application.properties eingegeben

---
+++ src/main/java/de/jonasermert/filmverwaltung/FilmverwaltungApplication.java
@@ -0,0 +1,13 @@
+package de.jonasermert.filmverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmverwaltungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmverwaltungApplication.class, args);
+    }
+
+}
